13133dd7f0b088eeb8d42fed31d6ed72115d1e02
diwasbk <diwasbk903@gmail.com>
2026-01-17 20:21:35 +0545

    chore(config): Add dotenv setup for environment variables

---
+++ config/config.js
@@ -0,0 +1,9 @@
+import dotenv from "dotenv"
+
+dotenv.config();
+
+export const PORT = process.env.PORT
+
+export const APP_PASS = process.env.APP_PASS
+
+export const USER_EMAIL = process.env.USER_EMAIL
