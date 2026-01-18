504d2aeb685996087df407ce60d9d5743d84c675
diwasbk <diwasbk903@gmail.com>
2026-01-18 18:41:12 +0545

    chore(server): Enable CORS for frontend

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 import express from "express"
 import { PORT } from "./config/config.js"
 import contactRouter from "./routes/contactRoutes.js";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
+app.use(cors({
+  origin: "http://localhost:3000",
+  credentials: true
+}));
 
 app.use("/api/contact", contactRouter);
 
